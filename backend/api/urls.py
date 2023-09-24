5c1853c91df203cca5492e2659cd44daf57358e2
Никита Болдырев <krul1wtd@yandex.ru>
2023-09-24 19:06:47 +0700

    fixes

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -13,28 +13,12 @@ router.register('tags', TagsViewSet)
 router.register('ingredients', IngredientViewSet)
 
 urlpatterns = [
-     path('users/subscriptions/', SubscriptionsApiView.as_view()),
-     path('', include(router.urls)),
-     path('', include('djoser.urls')),
-     path('auth/', include('djoser.urls.authtoken')),
-     path(
-          'users/<int:pk>/subscribe/',
-          subscribe,
-          name='subscribe'
-     ),
-     path(
-          'auth/token/login/',
-          TokenCreateView.as_view(),
-          name='login'
-     ),
-     path(
-          'auth/token/logout/',
-          TokenDestroyView.as_view(),
-          name='logout'
-     ),
-     path(
-          'users/set_password/',
-          set_password,
-          name='set_password'
-     ),
+    path('users/subscriptions/', SubscriptionsApiView.as_view()),
+    path('', include(router.urls)),
+    path('', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('users/<int:pk>/subscribe/', subscribe, name='subscribe'),
+    path('auth/token/login/', TokenCreateView.as_view(), name='login'),
+    path('auth/token/logout/', TokenDestroyView.as_view(), name='logout'),
+    path('users/set_password/', set_password, name='set_password'),
 ]
