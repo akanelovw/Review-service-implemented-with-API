9ab7a12227ba142df097728fb6e80a37d14aac4f
Никита Болдырев <krul1wtd@yandex.ru>
2023-10-01 10:01:10 +0700

    admin zone

---
+++ backend/recipes/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+
+from recipes.models import (Tag, Ingredient, IngredientMeasure,
+                            Favorite, ShoppingCart, Recipe)
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'name',
+        'cooking_time',
+        'text',
+        'tags',
+        'image',
+        'author',
+        'in_favorites'
+    )
+    list_editable = (
+        'name', 'cooking_time', 'tags',
+        'image', 'author'
+    )
+    readonly_fields = ('in_favorites',)
+    search_fields = ('name', 'author')
+    list_filter = ('tags',)
+
+    @admin.display(description='Тэги')
+    def tags(self, obj):
+        list_ = [_.name for _ in obj.tags.all()]
+        return ', '.join(list_)
+
+    @admin.display(description='В избранном')
+    def in_favorites(self, obj):
+        return obj.favorites.count()
+
+
+admin.site.register(Tag)
+admin.site.register(IngredientMeasure)
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
---
+++ backend/users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from users.models import Follow, User
+
+admin.site.register(Follow)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_superuser',
+    )
+    search_fields = ('email', 'username')
