ca8fb0367bf970d1d17cb0cf20f3b190030c0bf4
Никита Болдырев <krul1wtd@yandex.ru>
2023-10-07 20:01:01 +0700

    final

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -10,11 +10,11 @@ def create_cart(user):
     ingredients = ingredients.values(
         'ingredient__name', 'ingredient__measurement_unit'
     ).annotate(
-        ingredient=F('ingredient__name'),
+        name=F('ingredient__name'),
         measure=F('ingredient__measurement_unit'),
-        total_amount=Sum('amount')).order_by('-ingredient_name')
+        total_amount=Sum('amount')).order_by('-name')
     file_list = '\n'.join([
-        f"{field['ingredient']} - {field['total_amount']} {field['measure']}"
+        f"{field['name']} - {field['total_amount']} {field['measure']}"
         for field in ingredients
     ])
     return file_list
