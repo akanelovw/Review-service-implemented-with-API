888f2316785ff006b8bc9e308ea005debd7b900a
Никита Болдырев <krul1wtd@yandex.ru>
2023-10-07 19:47:06 +0700

    final

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -5,17 +5,16 @@ from recipes.models import IngredientMeasure
 
 def create_cart(user):
     ingredients = IngredientMeasure.objects.select_related(
-            'recipe', 'ingredient'
-        )
+        'recipe', 'ingredient'
+    )
     ingredients = ingredients.values(
         'ingredient__name', 'ingredient__measurement_unit'
-        ).annotate(
-            ingredient_name=F('ingredient__name'),
-            measure=F('ingredient__measurement_unit'),
-            total_amount=Sum('amount')
-        ).order_by('-ingredient_name')
+    ).annotate(
+        ingredient_name=F('ingredient__name'),
+        measure=F('ingredient__measurement_unit'),
+        total_amount=Sum('amount')).order_by('-ingredient_name')
     file_list = '\n'.join([
-        f"{item['ingredient_name']} - {item['total_amount']} {item['measure']}"
-        for item in ingredients
+        f"{field['ingredient_name']} - {field['total_amount']} {field['measure']}"
+        for field in ingredients
     ])
     return file_list
